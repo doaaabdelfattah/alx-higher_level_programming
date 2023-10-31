983fd8c90f31f5b78cd8fe6809eaf095055ac3ed
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-10-31 03:53:12 +0000

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+*check_cycle - check cycle
+*@list: pointer to node
+*Return: 0 if no cycle, 1 if else
+*/
+
+int check_cycle(listint_t *list)
+{
+listint_t *start;
+listint_t *temp;
+if (list == NULL)
+return (0);
+start = list->next;
+temp = list->next->next;
+
+while (start != NULL && temp != NULL)
+{
+if (start == temp)
+return (1);
+
+start = start->next;
+temp = temp->next->next;
+}
+return (0);
+}
