072cdeac2ecd17318fac2f8997bd8fefe04f431b
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-02 02:09:18 +0000

    task C

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * insert_node - function insert nu into sorted SL list
+ * @head: pointer to head pointer
+ * @number: number to be intered
+ * Return: address of the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *new_node, *tmp, *prev;
+tmp = prev = *head;
+if (*head == NULL)
+return (NULL);
+/* create the new node with the provided number */
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = number;
+while (tmp != NULL && tmp->n < number)
+{
+prev = tmp;
+tmp = tmp->next;
+}
+/* Connect the new node to the list */
+prev->next = new_node; /*left side*/
+new_node->next = tmp; /* right side*/
+return (new_node);
+}
