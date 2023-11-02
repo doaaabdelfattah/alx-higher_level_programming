300ffad185b69e9803a4a8bc8b9fb0cce4fed233
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-02 10:32:07 +0000

    “upload”

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,19 +5,20 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     argcount = len(sys.argv)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
     # Check arguments count
-    if argcount != 4:
+    if argcount - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+
     operators = ['+', '-', '*', '/']
     op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
     # Check the operator
     if op not in operators:
         print("Unknown operator. Available operators: +, -, * and / ")
-        exit(1)
+        sys.exit(1)
 
     # Calculate
     if op == '+':
