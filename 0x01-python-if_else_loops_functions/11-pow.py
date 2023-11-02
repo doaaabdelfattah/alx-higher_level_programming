7efe7de050c43e3c91350599ec02ab84e131d617
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-02 01:27:55 +0000

    fix

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    # if b == 0:
-    #     return 1
-    # elif b < 0:
-    #     return -1
-    # else:
-    #     pow = 0
-    #     while pow < b:
-    #         a = a * a
-    #         pow += pow
-    return a **b
+    return a ** b
