97f60f3fa7e00a2ce125d2fcf3ffc6085be8f414
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-02 01:35:43 +0000

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for i in range(0,101):
+        if ((i % 5 == 0) and (i % 3 == 0)):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
+        else:
+            print("{}".format(i), end="")
+        if (i != 100):
+            print(" ")
+    print("")
