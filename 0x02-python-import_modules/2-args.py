429efb36e16cb9c6f4e5c2555dba9e1d9a5e5afb
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-02 09:40:17 +0000

    “upload”

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Your code should not be executed when imported
+if __name__ == "__main__":
+    import sys
+
+allarg = len(sys.argv)
+if allarg == 1:
+    print("0 argument.")
+elif allarg == 2:
+    print("{} argument:".format(allarg - 1))
+else:
+    print("{} arguments:".format(allarg - 1))
+
+for x in range(1, allarg):
+    print("{}: {}".format(x, sys.argv[x]))
