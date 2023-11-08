14c635301377e02ec2d00fbf224bf6a0e7f8b4ad
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-08 05:42:27 +0000

    “upload”

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if matrix:
+        for i in range(len(matrix)):
+            new_row = []
+            for j in range(len(matrix[i])):
+                new_row.append(matrix[i][j] ** 2)
+            new_matrix.append(new_row)
+        return new_matrix
+    else:
+        return []
