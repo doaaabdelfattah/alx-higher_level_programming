e948242c4d30c43ba01d9015a9359085c086d451
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 20:25:22 +0000

    “upload”

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,3 +3,4 @@ def complex_delete(a_dictionary, value):
     keys_del = [key for key, values in a_dictionary.items() if values == value]
     for key in keys_del:
             del a_dictionary[key]
+    return a_dictionary
--- 0x04-python-more_data_structures/102-main.py
+++ 0x04-python-more_data_structures/102-main.py
@@ -5,13 +5,13 @@ print_sorted_dictionary = \
 
 a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
 new_dict = complex_delete(a_dictionary, 'C')
-print_sorted_dictionary(a_dictionary)
+print(a_dictionary)
 print("--")
-print_sorted_dictionary(new_dict)
+print(new_dict)
 
 print("--")
 print("--")
 new_dict = complex_delete(a_dictionary, 'c_is_fun')
-print_sorted_dictionary(a_dictionary)
+print(a_dictionary)
 print("--")
-print_sorted_dictionary(new_dict)
+print(new_dict)
