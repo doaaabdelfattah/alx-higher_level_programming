be368ecb562b79ad3947bafb417dbde033a63030
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 19:42:28 +0000

    “upload”

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_num = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    result = 0
+    for i in range(len(roman_string)):
+        value = roman_num[roman_string[i]]
+        if i < len(roman_string) - 1 and \
+                roman_num[roman_string[i + 1]] > value:
+            result -= value
+        else:
+            result += value
+    return result
