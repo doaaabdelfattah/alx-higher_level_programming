a1c4c5d87333cb9a855d66f76f3465ad4a3ceb9a
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 20:04:36 +0000

    “upload”

--- 0x04-python-more_data_structures/man.py
+++
@@ -1,4 +0,0 @@
-roman_to_int = __import__('pali').find_largest_palindrome
-
-result = find_largest_palindrome()
-print("The largest palindrome made from the product of two 3-digit numbers is:", result)
