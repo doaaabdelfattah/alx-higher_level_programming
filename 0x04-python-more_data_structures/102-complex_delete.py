8d3086e6fcafded1242adfe9986df17f6d73718e
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 20:23:38 +0000

    “upload”

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys_del = [key for key, values in a_dictionary.items() if values == value]
+    for key in keys_del:
+            del a_dictionary[key]
