30a044c71a7bf725ce1e023461d86eb4668aaf23
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-24 21:16:16 +0000

    “upload”

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module Definition"""
+
+
+class Square:
+    """This is Class Square definition
+    """
+
+    def __init__(self, size):
+        """Initialize Square object
+
+        Args:
+            size: square size(pivate)
+        """
+
+        self.__size = size
+    pass
