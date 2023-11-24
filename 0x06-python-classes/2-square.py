86fe7b3f5b13e54db641553134a8b5f5b52c88d5
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-24 21:28:46 +0000

    “upload”

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module Definition"""
+
+
+class Square:
+    """This is Class Square definition
+    """
+
+    def __init__(self, size=0):
+        """Initialize Square object
+
+        Args:
+            size: square size(pivate)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
