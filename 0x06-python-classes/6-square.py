226c3a2ea7ccec6d2eb4c440e3e4361146e49c2b
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-24 22:04:52 +0000

    “upload”

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module Definition"""
+
+
+class Square:
+    """This is Class Square definition
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize Square object
+
+        Args:
+            size: square size(pivate)
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def position(self):
+        """Retrieve position """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and len(value) == 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @property
+    def size(self):
+        """Get/Set the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return current square area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        for i in range(0, self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
