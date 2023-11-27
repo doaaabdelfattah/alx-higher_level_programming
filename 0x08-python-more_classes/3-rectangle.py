e707df8afd03994557c1c06f45a6da2cec439475
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-27 19:46:44 +0000

    “upload”

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -60,4 +60,6 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         else:
-            return ('\n'.join(['#' * self.__width for _ in range(self.__height)]))
+            return (
+                '\n'.join(['#' * self.__width for _ in range(self.__height)])
+                )
