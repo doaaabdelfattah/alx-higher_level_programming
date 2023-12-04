504280c8d5be2d0f90161a368a0e4de0b4337d1e
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-04 10:39:10 +0000

    “upload”

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" is_same_class Module """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Return True if: Same class or inherit from
+    """
+    return isinstance(obj, a_class)
