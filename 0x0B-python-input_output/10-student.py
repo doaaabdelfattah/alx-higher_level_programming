a61ff0e3ad2b14daf70026793cba7bbf239d91a1
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-06 10:24:49 +0000

    “upload”

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Student class '''
+
+
+class Student:
+    ''' studen class definition'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+    
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            return (self.first_name, self.last_name)
+        else:
+            return self.__dict__
