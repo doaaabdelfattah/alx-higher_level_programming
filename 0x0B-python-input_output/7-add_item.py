b88796a7d5a1613039b090b270187df40202728a
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-06 09:34:25 +0000

    “upload”

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+''' add items from arguments '''
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
-
-''' add items from arguments '''
 
 
 items = sys.argv[1:]
