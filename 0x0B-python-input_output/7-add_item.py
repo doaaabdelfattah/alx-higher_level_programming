fc48662d1001da1f6c08cbd00e9baa8c7e511c74
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-06 09:38:13 +0000

    “upload”

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,9 +5,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    load_from_json_file('add_item.json')
-except FileNotFoundError:
-    items = sys.argv[1:]
-    save_to_json_file(items, 'add_item.json')
-    # load_from_json_file('add_item.json')
+
+items = sys.argv[1:]
+save_to_json_file(items, 'add_item.json')
+load_from_json_file('add_item.json')
