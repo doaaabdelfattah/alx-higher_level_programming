4ec3bb2920d2237c7e62d06b585c09e315130ce6
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-06 09:03:47 +0000

    “upload”

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" write_file Function Module
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    ''' writes an Object to a text file, using a JSON representation'''
+    with open(filename, encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
