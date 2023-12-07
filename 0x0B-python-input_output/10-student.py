ad46463bbf850b58545919e1ae14fea67197a032
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-07 01:25:56 +0000

    “upload”

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,9 +11,16 @@ class Student:
 
     def to_json(self):
         return self.__dict__
-    
+
     def to_json(self, attrs=None):
-        if type(attrs) is list:
-            return (self.first_name, self.last_name)
+        if (type(attrs) is list):
+            # if True create Dictionary using dictionary comprehension
+            # 1) Iterate over each attribute name (K) in the list attrs
+            # 2) For each attribute, it checks if the object (self) \
+            # has that attribute using hasattr
+            # 3) if exist: it add key-value pair to the dict where:
+            #   a.Key is attribute
+            #   b.Value is the value of attribute
+            {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         else:
             return self.__dict__
