9a8bce7d9b67613af5dd92575bf08718db29ff56
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-07 01:55:44 +0000

    “upload”

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 ''' pascal_triangle Module '''
 
+
 def pascal_triangle(n):
     ''' pascal_triangle Module '''
     if n <= 0:
         return []
-    triangle = []
-    for row in range(n):
-        # Initialize the first row
-        if row == 0:
-            triangle.append[1]
-        else:
-            # Calculate subsequent rows
-            current = [1]
-            for i in range(1, row):
-                value = triangle[row - 1][i - 1] + triangle[row - 1][i]
-                current.append(value)
-            current.append(1)
-            triangle.append(current)
-    return triangle
+    else:
+        triangle = []
+        for row in range(n):
+            # Initialize the first row
+            if row == 0:
+                triangle.append[1]
+            else:
+                # Calculate subsequent rows
+                current = [1]
+                for i in range(1, row):
+                    value = triangle[row - 1][i - 1] + triangle[row - 1][i]
+                    current.append(value)
+                current.append(1)
+                triangle.append(current)
+        return triangle
