383f197f6a7d578dbd531267f5783e8b361615a0
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-12 00:37:20 +0000

    “upload_dir”

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     '''Class Rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''Initialize new rectangle'''
         super().__init__(id)
         self.height = height
         self.width = width
