2f7d732192ef2bbe24d42aea36aaabda634920b4
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-12 01:50:57 +0000

    “upload_dir”

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -26,49 +26,8 @@ class BaseTestCase(unittest.TestCase):
         b4 = Base(7)
         self.assertEqual(b4.id, 7)
         b5 = Base()
-        self.assertEqual(b1.id, 4)
-
-    def test_three_bases(self):
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 2)
-
-    def test_None_id(self):
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
-
-    def test_unique_id(self):
-        self.assertEqual(12, Base(12).id)
-
-    def test_nb_instances_after_unique_id(self):
-        b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
-
-    def test_id_public(self):
-        b = Base(12)
-        b.id = 15
-        self.assertEqual(15, b.id)
-
-    def test_nb_instances_private(self):
-        with self.assertRaises(AttributeError):
-            print(Base(12).__nb_instances)
-
-    def test_str_id(self):
-        self.assertEqual("hello", Base("hello").id)
-
-    def test_float_id(self):
-        self.assertEqual(5.5, Base(5.5).id)
-
-    def test_complex_id(self):
-        self.assertEqual(complex(5), Base(complex(5)).id)
-
-    def test_dict_id(self):
-        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
-
+        self.assertEqual(b5.id, 4)
+        
 
 if __name__ == "__main__":
     unittest.main()
