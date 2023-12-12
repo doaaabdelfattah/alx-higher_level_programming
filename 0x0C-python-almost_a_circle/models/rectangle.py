2d9ff8184f9a14faaeb20f56c84490608a10dab9
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-12 00:48:53 +0000

    “upload_dir”

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,10 @@ class Rectangle(Base):
 
     def __str__(self):
         '''string representation for class'''
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return (
+            f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
+            f"{self.width}/{self.height}"
+        )
 
     def display(self):
         ''' print Rectangle by character #'''
