6342e079e30cfc27dd4580cc559f70e940bb2c9a
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-12 01:38:22 +0000

    “upload_dir”

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,29 @@
+
+import io
+import sys
+import unittest
+from models.base import Base
+from models.square import Square
+
+
+class TestSquare_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Square class."""
+
+    def test_is_base(self):
+        self.assertIsInstance(Square(10), Base)
+
+    def test_is_rectangle(self):
+        self.assertIsInstance(Square(10), Square)
+
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Square()
+
+    def test_one_arg(self):
+        s1 = Square(10)
+        s2 = Square(11)
+        self.assertEqual(s1.id, s2.id - 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
