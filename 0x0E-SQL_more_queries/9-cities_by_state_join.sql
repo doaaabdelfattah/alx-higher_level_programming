f3f38fd81599bf9c909e548e0766857d459c2a3d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-01-18 22:58:23 +0000

    “Update”

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.id FROM hbtn_0d_usa
-ORDER BY cities.id ASC
+SELECT cities.id, cities.name, states.id 
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
