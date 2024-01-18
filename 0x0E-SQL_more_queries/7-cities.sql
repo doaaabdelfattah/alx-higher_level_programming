a1a340b867742a0eae66c42e32af3b45396f94ef
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-01-18 22:39:37 +0000

    “Update”

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,8 @@
 -- Cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL 
     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
