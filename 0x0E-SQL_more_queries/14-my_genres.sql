89caccbbf79dfaefec9fb491b6981b91b5ac2f74
“doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-08 22:55:10 +0000

    “Update”

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,7 @@
 -- lists all genres of the show Dexter.
 SELECT a.name
 FROM tv_genres AS a
-INNER JOIN tv_show_genres AS b
-ON a.id = b.genre_id
-WHERE b.show_id IN (
-SELECT id FROM tv_shows
-WHERE title = "Dexter")
+INNER JOIN tv_show_genres AS b ON a.id = b.genre_id
+INNER JOIN tv_shows AS c ON b.show_id = c.id
+WHERE c.title = "Dexter"
 ORDER BY a.name ASC;
