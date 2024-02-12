8ca04dace2867aad67abab8b053e8c07f889c4e0
“doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-12 21:41:23 +0000

    “Update”

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const VarOne = 'C is fun';
+const VarTwo = 'Python is cool';
+const VarThree = 'JavaScript is amazing';
+console.log(VarOne);
+console.log(VarTwo);
+console.log(VarThree);
