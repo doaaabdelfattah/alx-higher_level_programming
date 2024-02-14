a5c64aac0c3b3f607afa8f2833765024db7b5644
“doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-14 22:18:30 +0000

    “Update”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argument = process.argv;
+const number = parseInt(argument[2]);
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
