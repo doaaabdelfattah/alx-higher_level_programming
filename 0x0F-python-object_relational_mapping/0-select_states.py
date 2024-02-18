0fa91d016d1e902d492d822e32ee0ca2870537e2
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-18 07:35:25 +0000

    “upload”

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,14 @@
 #!/usr/bin/python3
-import MySQLdb
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Retrieve MySQL connection parameters from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Establish MySQL connection
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Create cursor object
-    cursor = db.cursor()
-
-    # Execute SELECT query to retrieve states, sorted by id in ascending order
-    # Use execute method to make query
-    cursor.execute('SELECT * FROM states ORDER BY id ASC')
-
-    # Fetching: retrieving data from a database after executing a SELECT query.
-    # fetchall(): retrieves all rows of a query result set as a list of tuples.
-    result = cursor.fetchall()
-
-    # Process the data
-    for row in result:
-        print(row)
-
-    # good practice to close the cursor to free up resources
-    cursor.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
+    
