7afb1bd0b4cc6b76b30c5c0b2afe0fc6436e61db
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-24 01:57:01 +0200

    “update”

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -3,40 +3,20 @@
 # Usage: ./11-model_state_insert.py <mysql username> /
 #                                   <mysql password> /
 #                                   <database name>
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker, scoped_session
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
+    new_state = State(name='Louisiana')
+    session.add(new_state)
     session.commit()
-    print(louisiana.id)
-
-# #!/usr/bin/python3
-# """Start link class to table in database
-# """
-# from model_state import Base, State
-# from sqlalchemy.orm import sessionmaker, scoped_session
-# import sys
-# from sqlalchemy import create_engine
-
-
-# if __name__ == '__main__':
-#     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-#                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
-#                            pool_pre_ping=True)
-
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     new_state = State(name='Louisiana')
-#     session.add(new_state)
-#     session.commit()
-#     print(new_state.id)
+    print(new_state.id)
