f36e202b4b4e1635c3b1e8fa99e45092dea0df60
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-24 01:21:23 +0200

    “update”

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker, scoped_session
+import sys
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    url = f'mysql+mysqldb://{user}:{passwd}@{host}/{db}'
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name.like('%a%')).all()
+
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
