b83abb205eae95c7866424404729b7fe6cba605f
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-24 00:47:13 +0200

    “update”

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,15 +6,16 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import sys
 from sqlalchemy import create_engine
 
-host='localhost'
-port=3306
-user=sys.argv[1]
-passwd=sys.argv[2]
-db=sys.argv[3]
-if __name__ == "__main__":
-#  # The string form of the URL is dialect[+driver]://user:password@host/dbname[?key=value..],
-    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@{host}/{db}', pool_pre_ping=True)
 
+if __name__ == "__main__":
+    
+    host='localhost'
+    port=3306
+    user=sys.argv[1]
+    passwd=sys.argv[2]
+    db=sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@{host}/{db}', pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     result = session.query(State).all().orderby(State.id)
