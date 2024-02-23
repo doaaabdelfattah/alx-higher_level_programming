5423aeaeab27efab8b5bf186b74a363ffabca2c3
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-24 01:45:18 +0200

    “update”

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,20 +6,13 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import sys
 from sqlalchemy import create_engine
 
-
 if __name__ == "__main__":
-
-    host = 'localhost'
-    port = 3306
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-
-    url = f'mysql+mysqldb://{user}:{passwd}@{host}/{db}'
-    engine = create_engine(url, pool_pre_ping=True)
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     found = False
     for state in session.query(State):
         if state.name == sys.argv[4]:
@@ -28,6 +21,20 @@ if __name__ == "__main__":
             break
     if found is False:
         print("Not found")
+
+# if __name__ == "__main__":
+
+#     host = 'localhost'
+#     port = 3306
+#     user = sys.argv[1]
+#     passwd = sys.argv[2]
+#     db = sys.argv[3]
+
+#     url = f'mysql+mysqldb://{user}:{passwd}@{host}/{db}'
+#     engine = create_engine(url, pool_pre_ping=True)
+
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
     # result = session.query(State)
     # a = False
     # for row in result:
