6e985b085c16350d1f18dae54fa4a0641e113a76
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-24 02:11:00 +0200

    “update”

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    state_to_update = session.query(State).filter_by(id=2).first()
-    # Update the name attribute
-    state_to_update.name = 'New Mexico'
+    for state in session.query(State):
+        if "a" in state.name:
+            session.delete(state)
     session.commit()
