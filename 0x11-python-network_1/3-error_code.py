b0cf54f8f457194a4ccf8937f8a4b7dae799351d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-02 00:56:01 +0200

    “task1”

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Response header value #0
+'''
+import urllib.request
+import urllib.parse
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read())
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
