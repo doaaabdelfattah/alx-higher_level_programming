08b1c5701fafd7a1169f4d20f2f0e23924118c80
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-02 01:55:18 +0200

    “task1”

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
         letter = sys.argv[1]
     else:
         letter = ""
