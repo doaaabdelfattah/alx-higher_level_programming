23a1d44fd8833b7f1b01bad038ed4f6edbc4a6bf
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-02 02:07:38 +0200

    “task1”

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,8 +3,11 @@
 '''
 import requests
 import sys
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    r = requests.post(url, data=email)
+    value = {'email': sys.argv[2]}
+
+    r = requests.post(url, data=value)
     print(r.text)
