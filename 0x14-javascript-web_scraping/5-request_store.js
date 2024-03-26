ee261ae77ba069f5b93cf822248986401942bbf8
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-26 04:26:36 -0700

    “upload”

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,15 +5,15 @@ const request = require('request');
 // Get the URL and file path from the command-line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
+// Create a writable stream for the file
+const writeStream = fs.createWriteStream(filePath, 'utf8');
 
 // Make a GET request to the specified URL
-request.get(url, (error, response, body) => {
-    // Write the response body to the file
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-            return;
-        }
+request.get(url)
+    .on('error', (err) => {
+        console.error('Error fetching webpage:', err);
+    })
+    .pipe(writeStream)
+    .on('finish', () => {
         console.log('File saved successfully:', filePath);
     });
-});
