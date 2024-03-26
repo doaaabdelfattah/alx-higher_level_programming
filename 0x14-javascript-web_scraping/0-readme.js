04859bbeb654aa01f89f2227b4d344f8b5e8f62a
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-26 03:09:00 -0700

    “upload”

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
-
 const fs = require('fs');
 // Get the file path from command-line arguments
 const filePath = process.argv[2];
 
 // Asynchronous method
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
