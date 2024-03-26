dd3747d73b8ab9c29253d3735252fe08f9a596b3
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-26 03:34:47 -0700

    “upload”

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Make a Get request to URL
+const url = process.argv[2];
+request.get(url, (error, responce) => {
+  if(error) {
+    console.error('Error', error);
+  } else {
+	  console.log('code:', responce.statusCode);
+  }
+});
