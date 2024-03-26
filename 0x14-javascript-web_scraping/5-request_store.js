d3a124d74cfcd180b57066de93dffa875afd88c5
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-26 04:30:21 -0700

    “upload”

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,4 @@
 
 const fs = require('fs');
 const request = require('request');
-// Get the URL and file path from the command-line arguments
-const url = process.argv[2];
-const filePath = process.argv[3];
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
